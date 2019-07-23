03bcfbe6e54ea4e4d4d58c98b0db92f743fe6fe7
Alfred-kai <wangkaihpu@sina.com>
2019-07-23 19:29:54 +0800

    test

--- README.md
+++
@@ -1,2 +0,0 @@
-# vue-easy-nav
-a simple vue navigation component
--- src/components/index.js
+++
@@ -1,5 +0,0 @@
-import VueEasyNav from "./VueEasyNav.vue";
-
-VueEasyNav.install = Vue => Vue.component(VueEasyNav.name, VueEasyNav);
-
-export default VueEasyNav;
